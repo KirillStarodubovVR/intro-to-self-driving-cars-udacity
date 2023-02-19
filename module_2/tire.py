8b047c2e9e70845d8c35738671c25367d092cd7a
KirillStarodubov <starkirill90@mail.ru>
2023-02-19 21:09:59 +0400

    update

--- module_2/tire.py
+++ module_2/tire.py
@@ -2,7 +2,7 @@ from math import pi
 
 """For Carla, that could mean doing the following every time she "wakes up":
 
-Measure the distance between herself and an object directly behind her.
+1. Measure the distance between herself and an object directly behind her.
 Turn her wheels exactly one full rotation.
 Make the same distance measurement again.
 Use the measurements from 1 and 3 to calculate the distance traveled. This is the current circumference of her tires.
@@ -11,7 +11,10 @@ Unfortunately, Carla has a bug in the code that performs steps 4 and 5! Help Car
 
 def get_tire_diameter(dist_before_turn, dist_after_turn):
     # TODO - there's a bug in this function! Find and fix it!
-
+    # Calculate the length (circumference) of the tire
     circumference = dist_after_turn - dist_before_turn
-    diameter = circumference * pi
+    # L = 2 * pi * R - the length of the circle
+    # L = 2 * pi * D/2 = pi * D - the length of the circle over diameter
+    # D = L / pi
+    diameter = circumference / pi
     return diameter
